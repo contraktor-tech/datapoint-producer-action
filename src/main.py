6dda4ff01c66b19227be2b3bbb91ef5dca523b56
RobsonAndradev <robsom.s.a@gmail.com>
2021-10-20 10:54:34 -0300

    Use one bucket to deploy and rollback

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 token = os.environ["INFLUX_TOKEN"]
-bucket = os.environ.get("INFLUX_BUCKET", "deployment")
+measurement = os.environ.get("INFLUX_BUCKET", "deploy")
 influxdb_url = os.environ["INFLUX_URL"]
 app_name = os.environ["APP"]
 org = os.environ.get("INFLUX_ORG", "Contraktor")
+bucket = 'deployment'
 
 influxdb = InfluxDBClient(url=influxdb_url, token=token)
 
-point = Point(bucket)
+point = Point(measurement)
 point.field(app_name, 1)
 point.time(datetime.utcnow(), WritePrecision.NS)
 
